b44859da01a0d9b4112d79007747b6fdd2479c25
zyf <834917014@qq.com>
2022-11-03 14:33:45 +0800

    电子木鱼青春版V1

---
+++ app/src/main/java/com/zyf/electronicwoodfish/ShareUtil.kt
@@ -0,0 +1,34 @@
+package com.zyf.electronicwoodfish
+
+import android.content.Context
+import android.content.SharedPreferences
+
+
+/**
+ * @author zengyifeng
+ * @date createDate:2022-11-01
+ * @brief description
+ */
+object ShareUtil {
+ private var sps: SharedPreferences?=null
+ private fun getSps(context: Context):SharedPreferences{
+  if(sps==null){
+   sps=context.getSharedPreferences("default",Context.MODE_PRIVATE)
+  }
+  return sps!!
+ }
+ fun putString(key:String,value:String?,context:Context){
+  if(!value.isNullOrBlank()){
+   val editor:SharedPreferences.Editor=getSps(context).edit()
+   editor.putString(key,value)
+   editor.apply()
+  }
+ }
+ fun getString(key:String,context:Context):String?{
+  if(key.isNotBlank()){
+   val sps:SharedPreferences=getSps(context)
+   return sps.getString(key,null)
+  }
+  return null
+ }
+}
